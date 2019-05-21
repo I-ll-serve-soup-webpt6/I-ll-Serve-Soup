39c808b9aad47b0868bb896aa820e013681051c5
slaforsjov <andrewj12763@gmail.com>
2019-05-21 17:29:57 -0600

    finishing JS

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-// We're supposed to only have one ID type reference per page.  I don't know why YET bc I haven't looked
-// into it deeper.  Also we have to build components in order to meet MVP.
-
-// let about = document.getElementById("aboutCard");
-// let market= document.getElementById("marketCard");
-// let sales = document.getElementById("salesCard");
-// let contact = document.getElementById("contactCard");
-
-
-
 class TabLink {
     constructor(element) {
       this.element = element;
@@ -37,32 +27,3 @@ class TabLink {
   }
   let links = document.querySelectorAll(".tabs-link").forEach(tab => new TabLink(tab));
 
-/*
-document.getElementById("about").addEventListener("click", function() {
-    about.classList.replace("closed", "open");
-    market.classList.replace("open", "closed");
-    sales.classList.replace("open", "closed");
-    contact.classList.replace("open", "closed");
-  });
-
-document.getElementById("market").addEventListener("click", function() {
-    market.classList.replace("closed", "open");
-    about.classList.replace("open", "closed");
-    sales.classList.replace("open", "closed");
-    contact.classList.replace("open", "closed");
-  });
-
-document.getElementById("sales").addEventListener("click", function() {
-    sales.classList.replace("closed", "open");
-    about.classList.replace("open", "closed");
-    market.classList.replace("open", "closed");
-    contact.classList.replace("open", "closed");
-  });
-
-document.getElementById("contact").addEventListener("click", function() {
-    contact.classList.replace("closed", "open");
-    sales.classList.replace("open", "closed");
-    about.classList.replace("open", "closed");
-    market.classList.replace("open", "closed");
-  });
-*/
