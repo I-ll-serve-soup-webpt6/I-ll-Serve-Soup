d6fc7e053c893ab67abc98fd72a7940f38e994a2
slaforsjov <andrewj12763@gmail.com>
2019-05-21 17:19:27 -0600

    Adding classes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,41 @@
 // We're supposed to only have one ID type reference per page.  I don't know why YET bc I haven't looked
 // into it deeper.  Also we have to build components in order to meet MVP.
 
-let about = document.getElementById("aboutCard");
-let market= document.getElementById("marketCard");
-let sales = document.getElementById("salesCard");
-let contact = document.getElementById("contactCard");
+// let about = document.getElementById("aboutCard");
+// let market= document.getElementById("marketCard");
+// let sales = document.getElementById("salesCard");
+// let contact = document.getElementById("contactCard");
+
+
+
+class TabLink {
+    constructor(element) {
+      this.element = element;
+      this.data = this.element.dataset.tab
+      this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+      this.tabItem =  new TabItem(this.itemElement);
+      this.element.addEventListener('click', () => {
+          this.select()
+      })
+    };
+  
+    select() {
+      const links = document.querySelectorAll(".tabs-link").forEach(link => link.classList.remove('tabs-link-selected'));
+      this.element.classList.toggle('tabs-link-selected');
+      this.tabItem.select();
+    }
+  }
+  
+  class TabItem {
+    constructor(element) {
+      this.element = element;
+    }
+    select() {
+      const items = document.querySelectorAll('.tabs-item').forEach(item => item.classList.remove('tabs-item-selected'));
+      this.element.classList.add('tabs-item-selected');
+    }
+  }
+  let links = document.querySelectorAll(".tabs-link").forEach(tab => new TabLink(tab));
 
 /*
 document.getElementById("about").addEventListener("click", function() {
